72996503b35cbfa8dbdf21559bfcd80628494cae
Pavel Korsakov <django@code.travel>
2021-08-26 20:44:52 +0700

    extractor.py

--- postgres_to_es/extractor.py
+++ postgres_to_es/extractor.py
@@ -1,52 +1,36 @@
 import logging
+
 from psycopg2.extensions import connection as _connection
 from psycopg2.extras import RealDictCursor
+
 from models import AbstractExtractor
 
+logging.basicConfig(filename="etl.log", level=logging.INFO)
 logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 class PostgresExtractor(AbstractExtractor):
-
     data = []
 
     def __init__(self, pg_conn: _connection):
         self.conn = pg_conn
         self.cursor = pg_conn.cursor(cursor_factory=RealDictCursor)
 
-    def get_data(self):
+    def get_data(self, table, column, pointer_begin_date, pointer_end_date):
         """
-        Получает не проиндексированные фильмы с сопутствующими данными
+        Получает не проиндексированные фильмы
         :return: результат выборки
         """
-        sql = """
-            SELECT * 
-            FROM public."content.film_work"
-            WHERE not indexed
-            ORDER BY id ASC LIMIT 100
-                 
+        sql = f"""
+            SELECT
+                id,
+                {column} 
+            FROM public."content.{table}" d 
+            WHERE d.modified <= to_date( '{str(pointer_end_date)}', 'YYYY-MM-DD HH24:MI:SS' ) AND d.modified > to_date( '{str(pointer_begin_date)}', 'YYYY-MM-DD HH24:MI:SS' )
         """
 
         self.cursor.execute(sql)
         self.data = self.cursor.fetchall()
 
-        logger.info(f'Extracted {len(self.data)}')
         return self.data
-
-    def set_index(self):
-        """
-        После загрузки устанавливается признак индексации
-        Снятие индексации происходит во время обновления записей в админке
-        :return:
-        """
-        data = ','.join(self.cursor.mogrify('%s', (item['id'],)).decode()
-                        for item in self.data)
-        sql = f"""
-            UPDATE public."content.film_work"
-            SET indexed = True
-            WHERE id in ({data});
-        """
-
-        self.cursor.execute(sql, (data, ))
-        self.conn.commit()
-        logger.info(f'Indexed {len(self.data)}')
