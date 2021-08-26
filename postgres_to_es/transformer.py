d9569d8702dcf435f8cd801b801de3811ee37555
Pavel Korsakov <django@code.travel>
2021-08-26 20:54:02 +0700

    transformer.py

--- postgres_to_es/transformer.py
+++ postgres_to_es/transformer.py
@@ -3,8 +3,7 @@ from models import AbstractTransformer
 
 class Transformer(AbstractTransformer):
 
-    def transform(self, row) -> dict:
-        pass
+    def transform(self, row, column) -> dict:
         """
         Преобразование названий полей в структуру для elastic search
         :param row: поле для преобразования
@@ -12,15 +11,6 @@ class Transformer(AbstractTransformer):
         """
 
         return {
-                'id': row['id'],
-                'title': row['title'],
-                'description': row['description'],
-                # 'creation_date': row['creation_date'],
-                'certificate': row['certificate'],
-                'file_path': row['file_path'],
-                'rating': row['rating'],
-                'type': row['type'],
-                'mpaa_rating': row['mpaa_rating'],
-                # 'created': row['created'],
-                # 'modified': row['modified'],
+                'id': row.get('id'),
+                f'{column}': row.get(column)
             }
